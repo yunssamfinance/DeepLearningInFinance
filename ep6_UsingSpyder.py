2f12f8cb4bb17284fb5756f958b6481183b4a4a3
yunssamfinance <97676460+yunssamfinance@users.noreply.github.com>
2022-01-18 07:52:13 -0500

    Python Files for Episodes 5 to 9.

---
+++ ep6_UsingSpyder.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3, 4])
+plt.ylabel('some numbers')
+plt.show()
